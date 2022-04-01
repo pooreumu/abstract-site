68e6252b2a7c158357b00cd4800c815bffa51fbf
pooreumu <pooreum83@gmail.com>
2022-04-01 23:25:55 +0900

    5번 테스트까지

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        for (const i of this.boards) {
+            if (i.name === board.name) {
+                throw new Error();
+            }
+        }
+
+        return this.boards.push(board);
+    }
+
+    findBoardByName(boardName) {
+        for (const i of this.boards) {
+            if (i.name === boardName) {
+                return i;
+            }
+        }
+        return false;
+    }
+}
+
+class Board {
+    constructor(name) {
+        if (name === '') {
+            throw new Error();
+        }
+        if (name === null) {
+            throw new Error();
+        }
+        this.name = name;
+    }
+}
 
 class Article {}
 
