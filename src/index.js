a04306fbf83ed5d5a70348fe5cd0fe27e39c1cd7
pooreumu <pooreum83@gmail.com>
2022-04-02 09:06:22 +0900

    class Board, class Article, class Comment 구현

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ class Site {
                 throw new Error();
             }
         }
-
-        return this.boards.push(board);
+        board.addBoardConfirm = true;
+        this.boards.push(board);
     }
 
     findBoardByName(boardName) {
@@ -32,12 +32,70 @@ class Board {
             throw new Error();
         }
         this.name = name;
+        this.addBoardConfirm = false;
+        this.articles = [];
+    }
+
+    publish(article) {
+        if (this.addBoardConfirm === false) {
+            throw new Error();
+        }
+        article.id = `${this.name}-${Math.random()}`;
+        const date = new Date();
+        const iso = date.toISOString();
+        article.createdDate = iso;
+        article.publishConfirm = true;
+        this.articles.push(article);
+    }
+
+    getAllArticles() {
+        return this.articles;
     }
 }
 
-class Article {}
+class Article {
+    constructor({ subject, content, author }) {
+        if (subject === null || subject === '') {
+            throw new Error();
+        } else if (content === null || content === '') {
+            throw new Error();
+        } else if (author === null || author === '') {
+            throw new Error();
+        }
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.publishConfirm = false;
+        this.comments = [];
+    }
 
-class Comment {}
+    reply(comment) {
+        if (this.publishConfirm === false) {
+            throw new Error();
+        }
+        const date = new Date();
+        const iso = date.toISOString();
+        comment.createdDate = iso;
+        this.comments.push(comment);
+    }
+
+    getAllComments() {
+        return this.comments;
+    }
+}
+
+class Comment {
+    constructor({ content, author }) {
+        if (content === null || content === '') {
+            throw new Error();
+        }
+        if (author === null || author === '') {
+            throw new Error();
+        }
+        this.content = content;
+        this.author = author;
+    }
+}
 
 module.exports = {
     Site,
